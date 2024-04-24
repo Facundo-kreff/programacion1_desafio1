88faac2e0c9a6d409e9f44f77fea3481b0ca008d
Facundo kreff <krefffacundo@gmail.com>
2024-04-24 01:03:38 -0300

    Desadio 12

--- Desafio_1.py
+++ Desafio_1.py
@@ -170,21 +170,46 @@ while puntosA != 3 and puntosB != 3:
 tenistas = []
 for i in range (8):
     tenistas.append(input("Jugador "+ str(1+i)+": "))
-
-print(tenistas)
-ronda = 0
 ganadores=[]
 i = 0
-while i == 0: 
-    ronda+=1
-    print("Ronda", ronda)
-    print(tenistas[i], tenistas[i+1])
-    partida1 = input("a." + str(tenistas[i] + " - b." + str(tenistas[i+1])))
-    if partida1 == "a":
-        ganadores.append(tenistas[i])
+while i < len(tenistas) and len(tenistas) != 1: 
+    if len(tenistas) > 4:
+        if i == 0:
+            print("\nRonda 1")
+        partida = input("a." + str(tenistas[i] + " - b." + str(tenistas[i+1])+ ": "))
+        if partida == "b":
+            tenistas.remove(tenistas[i])
+            i+=1
+            if i == 4:
+                i=0
+        else:
+            tenistas.remove(tenistas[i+1])
+            i+=1
+            if i == 4:
+                i=0
+    elif len(tenistas) > 2:
+        if i == 0:
+            print("\nRonda 2")
+        partida = input("a." + str(tenistas[i] + " - b." + str(tenistas[i+1])+ ": "))
+        if partida == "b":
+            tenistas.remove(tenistas[i])
+            i+=1
+            if i == 2:
+                i=0
+        else:
+            tenistas.remove(tenistas[i+1])
+            i+=1
+            if i == 2:
+                i=0
     else:
-        ganadores.append(tenistas[i+1])
-
+        if i == 0:
+            print("\nRonda 3")
+        partida = input("a." + str(tenistas[i] + " - b." + str(tenistas[i+1])+ ": "))
+        if partida == "b":
+            tenistas.remove(tenistas[i])
+        else:
+            tenistas.remove(tenistas[i+1])
+print("\n\nCampeon:", tenistas[0])
     
 
 
